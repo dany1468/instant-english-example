4a576beec42bf6a08f8c3eee589f917650331e02
dany1468 <dany1468@gmail.com>
2024-09-22 16:15:54 +0900

    add some comments

--- public/app.js
+++ public/app.js
@@ -1,11 +1,20 @@
-let displayEnglishDelay = 5000;
-let nextSentenceDelay = 10000;
-let sentencePairs = [];
-let currentIndex = 0;
-let isPaused = false;
-let autoPlayTimer;
-let quizStarted = false; // クイズが開始されたかどうか
+// 設定値 (ユーザーに設定できるようにしても良い)
+const displayEnglishDelay = 5000;
+const nextSentenceDelay = 10000;
 
+// CSVファイルから読み込んだ問題のペア
+let sentencePairs = [];
+// 現在再生中の問題のインデックス
+let currentIndex = 0;
+// 一時停止中かどうか
+let isPaused = false;
+
+// 自動再生の timer インスタンス
+let autoPlayTimer;
+// クイズが開始されたかどうか
+let quizStarted = false; 
+
+// ファイル選択時のイベントリスナー
 document.getElementById('fileInput').addEventListener('change', function(e) {
     const file = e.target.files[0];
     if (file) {
@@ -22,8 +31,8 @@ document.getElementById('fileInput').addEventListener('change', function(e) {
     }
 });
 
+// 開始ボタンのイベントリスナー
 document.getElementById('startButton').addEventListener('click', () => {
-    quizStarted = true;
     // 開始ボタンを非表示
     document.getElementById('startButton').style.display = 'none';
     // 操作ボタンを表示
@@ -33,32 +42,36 @@ document.getElementById('startButton').addEventListener('click', () => {
     document.getElementById('pauseButton').disabled = false;
     document.getElementById('nextButton').disabled = false;
 
+    // iPhone Safari で音声合成を有効化
     forceEnableSpeechForiPhone();
 
-    // クイズを開始
+    // 1秒後にクイズを開始
     setTimeout(startQuiz, 1000);
 });
 
+// CSVデータをパースして問題のペアを生成
+// CSVデータの形式: "日本語","英語" を想定しており、引用符内に引用符が含まれるケースは想定していない
 function parseCSV(data) {
+    // CSVデータを改行で分割
     const lines = data.split('\n');
+
+    // 全行を走査して問題のペアを生成
     for (let line of lines) {
-        if (!line.trim()) continue;
+        if (!line.trim()) continue; // 空行はスキップ
 
         const values = [];
         let value = '';
-        let insideQuote = false;
+        let insideQuote = false; // 現在が引用符(")の内部かどうか
 
+        // 1行に対して、1文字ずつ走査
         for (let i = 0; i < line.length; i++) {
             const char = line[i];
 
+            // 引用符が出現すれば、内部/外部を切り替える
             if (char === '"') {
-                if (insideQuote && line[i + 1] === '"') {
-                    value += '"';
-                    i++;
-                } else {
-                    insideQuote = !insideQuote;
-                }
-            } else if (char === ',' && !insideQuote) {
+                insideQuote = !insideQuote;
+            // 引用符の外部でカンマが出現した場合は、値を区切る
+            } else if (char === ',' && !insideQuote) { 
                 values.push(value);
                 value = '';
             } else {
@@ -77,7 +90,9 @@ function parseCSV(data) {
     }
 }
 
+// クイズを開始する
 function startQuiz() {
+    quizStarted = true;
     showSentence();
 }
 
@@ -87,38 +102,42 @@ function showSentence() {
         return;
     }
 
+    // 最後の問題までいけば最初に戻る
     if (currentIndex >= sentencePairs.length) {
         currentIndex = 0;
-    } else if (currentIndex < 0) {
+    } else if (currentIndex < 0) { // 「前へ」で最初の問題より戻った場合は最後へ
         currentIndex = sentencePairs.length - 1;
     }
 
+    // 現在の問題の日本語を表示
     const pair = sentencePairs[currentIndex];
     document.getElementById('japaneseSentence').textContent = pair.japanese;
     document.getElementById('englishSentence').style.display = 'none';
 
+    // 日本語を音声合成して再生
     speakText(pair.japanese, 'ja-JP');
 
     // 進捗状況を更新
     updateProgress();
 
     // 既存のタイマーをクリア
-    clearTimeout(window.displayTimeout);
-    clearTimeout(autoPlayTimer);
+    clearAllTimer();
 
     // 一定時間後に英文を表示
     window.displayTimeout = setTimeout(() => {
         document.getElementById('englishSentence').textContent = pair.english;
         document.getElementById('englishSentence').style.display = 'block';
-        speakText(pair.english, 'en-US');
-    }, 5000); // 5秒後に英文を表示
 
-    // 自動再生が有効な場合のみ次の問題に進むタイマーを設定
+        // 英文を音声合成して再生
+        speakText(pair.english, 'en-US');
+    }, displayEnglishDelay);
+
+    // 一時停止でない場合のみ自動再生タイマーを設定
     if (!isPaused) {
         autoPlayTimer = setTimeout(() => {
             currentIndex++;
             showSentence();
-        }, 10000); // 10秒後に次の問題へ
+        }, nextSentenceDelay)
     }
 }
 
@@ -129,17 +148,24 @@ function updateProgress() {
 // ボタンのイベントリスナー
 document.getElementById('nextButton').addEventListener('click', () => {
     currentIndex++;
-    clearTimeout(window.displayTimeout);
-    clearTimeout(autoPlayTimer);
+    
+    clearAllTimer();
+    
     showSentence();
 });
 
 document.getElementById('prevButton').addEventListener('click', () => {
     currentIndex--;
-    clearTimeout(window.displayTimeout);
-    clearTimeout(autoPlayTimer);
+
+    clearAllTimer();
+    
     showSentence();
 });
+
+function clearAllTimer() {
+    clearTimeout(window.displayTimeout);
+    clearTimeout(autoPlayTimer);
+}
 
 const pauseButton = document.getElementById('pauseButton');
 pauseButton.addEventListener('click', () => {
@@ -147,26 +173,32 @@ pauseButton.addEventListener('click', () => {
         // 自動再生を再開
         isPaused = false;
         pauseButton.textContent = '一時停止';
+
         showSentence();
     } else {
         // 自動再生を停止
         isPaused = true;
         pauseButton.textContent = '再開';
-        // タイマーをクリア
-        clearTimeout(window.displayTimeout);
-        clearTimeout(autoPlayTimer);
+        
+        clearAllTimer();
     }
 });
 
 
 function speakText(text, lang) {
     const utterance = new SpeechSynthesisUtterance(text);
+    
+    // lang に対応する Voice を取得
+    // 現在はとりあえず先頭にしているが、選択肢を表示してユーザーに選ばせても良さそう
     const voicesList = window.speechSynthesis.getVoices();
     utterance.voice = voicesList.find((voice) => voice.lang === lang);
     utterance.lang = lang;
+    
     speechSynthesis.speak(utterance);
 }
 
+// iPhone Safari で音声合成を有効化する
+// iOS Safari はユーザーの操作が必要なため、一度ダミー音声合成を実行する必要がある
 function forceEnableSpeechForiPhone() {
     // ダミー音声合成を実行
     var speechSynthesis = window.speechSynthesis;
